01a4c5f1e40a0435abc5207e7d2e039e05fe6948
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-07 23:54:17 +0100

    Adding google map API

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+function initMap() {
+    // pick center coordinates for your map
+    var losAngeles = {
+        lat: 34.063380, 
+        lng: -118.358080
+    };
+
+    // create map and say which HTML element it should appear in
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: losAngeles,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+    });
+}
