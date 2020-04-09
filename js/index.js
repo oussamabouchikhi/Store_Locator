8aa705193cbd253e10bd45f66cf27c05f7eeed64
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-09 17:18:03 +0100

    display stores data dynamically

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 
+window.onload = function() {
+    displayStores();
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
 function initMap() {
     // pick center coordinates for your map
     var losAngeles = {
@@ -13,11 +21,48 @@ function initMap() {
         mapTypeId: 'roadmap',
     });
 
-    // create marker and set its position
-    var marker = new google.maps.Marker({
-        map: map,
-        position: losAngeles,
-        title: 'losAngeles'
-    });
 
 }
+
+
+function displayStores(){
+    var storesHtml = '';
+    for(var [index, store] of stores.entries()){
+        var address = store['addressLines'];
+        var phone = store['phoneNumber'];
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>${address[0]}</span>
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="store-phone-number">${phone}</div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        ${index+1}
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+// Show stores markers on the map
+function showStoresMarkers(){
+    var bounds = new google.maps.LatLngBounds();
+    for(var [index, store] of stores.entries()){
+        var latlng = new google.maps.LatLng(
+            store["coordinates"]["latitude"],
+            store["coordinates"]["longitude"]);
+        var name = store["name"];
+        var address = store["addressLines"][0];
+
+        bounds.extend(latlng);
+        createMarker(latlng, name, address, index+1)
+    }
+    map.fitBounds(bounds);
+}
+
